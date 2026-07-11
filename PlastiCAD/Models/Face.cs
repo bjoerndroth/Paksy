5d7250b92b24adead84a44bf04958c15fc3f0365
Bjoern <bjoern@drahtkunst.de>
2026-07-11 04:08:33 +0200

    face

---
+++ PlastiCAD/Models/Face.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlastiCAD.Models
+{
+    public enum Face
+    {
+        Left,
+        Right,
+        Top,
+        Bottom,
+        Front,
+        Back
+    }
+}
